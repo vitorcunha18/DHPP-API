b5827d10c4124d13b7fdecdbd7db56cc7df2b2f2
Vitor Daniel do Nascimento Cunha <sonda.vitordn@tj.ac.gov.br>
2024-11-19 09:31:33 -0500

    network local

--- main.py
+++ main.py
@@ -27,6 +27,6 @@ app.include_router(index_router, prefix=informacao_api["version_prefix"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info", reload=True)
     
-    # uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
